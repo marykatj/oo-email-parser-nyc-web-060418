2fe21ed316378ac42fda08d77ccfecaca4d30b2f
Mary-Kate James <marykatj@gmail.com>
2018-06-09 19:44:45 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,7 +5,7 @@ attr_accessor :unformatted_emails
 @@all = []
 
 def initialize(unformatted_emails)
-  @unformatted_emails = unformatted_emails.split(/\,|\ /).join("")
+  @unformatted_emails = unformatted_emails.split(/\,|\ /).join(" ")
   @@all << @unformatted_emails
 end
 
